e552d82c38c406f69a9eb38051120e5677fb68f3
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2026-06-15 20:17:37 -0600

    Create boxplot for k=3 set 1

--- boxplot.py
+++ boxplot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_excel('compare_results_2_nodes.xlsx', sheet_name='gaps')
+df = pd.read_excel('compare_results_3_nodes.xlsx', sheet_name='gaps')
 
 sns.set_theme(style="whitegrid")
 plt.figure(figsize=(9, 6))
@@ -11,10 +11,10 @@ plt.figure(figsize=(9, 6))
 ax = sns.boxplot(data=df, palette="Blues", showmeans=True,
                  meanprops={"marker":"x", "markerfacecolor":"red", "markeredgecolor":"red", "markersize": 8})
 
-plt.title('Optimality Gap Distribution by Tree Size for instance set 1, k=2', fontsize=14, pad=15, fontweight='bold')
+plt.title('Optimality Gap Distribution by Tree Size for instance set 1, k=3', fontsize=14, pad=15, fontweight='bold')
 plt.xlabel('Tree Size (Number of Nodes)', fontsize=12, labelpad=10)
 plt.ylabel('Optimality Gap (%)', fontsize=12, labelpad=10)
 
 plt.tight_layout()
-plt.savefig('boxplot_optimality_gap_2_nodes.png', dpi=300)
+plt.savefig('boxplot_optimality_gap_3_nodes.png', dpi=300)
 plt.show()
