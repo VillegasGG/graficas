aac32e088f1b1686bcd2d95fdb2fd5ebaef570c2
Getsemani Villegas <gvillegasg1601@alumno.ipn.mx>
2026-06-15 20:13:57 -0600

    generating boxplots

---
+++ boxplot.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_excel('compare_results_2_nodes.xlsx', sheet_name='gaps')
+
+sns.set_theme(style="whitegrid")
+plt.figure(figsize=(9, 6))
+
+
+ax = sns.boxplot(data=df, palette="Blues", showmeans=True,
+                 meanprops={"marker":"x", "markerfacecolor":"red", "markeredgecolor":"red", "markersize": 8})
+
+plt.title('Optimality Gap Distribution by Tree Size for instance set 1, k=2', fontsize=14, pad=15, fontweight='bold')
+plt.xlabel('Tree Size (Number of Nodes)', fontsize=12, labelpad=10)
+plt.ylabel('Optimality Gap (%)', fontsize=12, labelpad=10)
+
+plt.tight_layout()
+plt.savefig('boxplot_optimality_gap_2_nodes.png', dpi=300)
+plt.show()
